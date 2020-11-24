517e3309629c04d3a6dcf78c1d7ee7638e08b86a
Pedro Rolo <pedrorolo@live.com.pt>
2020-11-24 18:31:50 +0000

    fragment pontos turisticos details

---
+++ app/src/main/java/helloworld/amsi/ipleiria/cultravel/vistas/PontoTuristicoDetailsFragment.java
@@ -0,0 +1,30 @@
+package helloworld.amsi.ipleiria.cultravel.vistas;
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import helloworld.amsi.ipleiria.cultravel.R;
+
+public class PontoTuristicoDetailsFragment extends Fragment {
+
+    public PontoTuristicoDetailsFragment() {
+        // Required empty public constructor
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_ponto_turistico_details, container, false);
+    }
+}
